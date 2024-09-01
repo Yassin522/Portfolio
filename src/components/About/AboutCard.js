fb72f115c3fa805afb5f1f9e39bb81180607fa88
YassinAbdulmahdi5 <yasin@seventhgenerationtech.com>
2024-09-01 12:21:09 +0300

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,9 +10,8 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Yassin Abdulmahdi </span>
             from <span className="purple"> Damascus, Syria.</span>
-            <br /> I am a final year student pursuing a Bachelor
-            in  Information Technology Engineering at Damascus University.
-           
+            <br /> I recently graduated with a Bachelor's degree in Information Technology Engineering from Damascus University.
+
             <br />
             I am open to work in any opportunity related to
           </p>
@@ -23,13 +22,13 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Frontend developement
             </li>
-            
+
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-         
+
         </blockquote>
       </Card.Body>
     </Card>
